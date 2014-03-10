0f07180214b52993d852471c223a8e76d90344a6
Tycoonman500 <tom@tycoonman.com>
2014-03-10 11:22:55 +1100

    unchanged admin checking in sAnnounce.lua, fixed a few silly mistakes

--- server/sAnnounce.lua
+++ server/sAnnounce.lua
@@ -14,14 +14,14 @@ end
 
 function Announce:Admin(target)
 	for k, v in pairs(self.users) do
-		user = v
+		local user = v
+		print(user)
 		if string.match(user, tostring(target:GetSteamId())) then
 			return true
 		end
-		return false
 	end
+	return false
 end
-
 
 function Announce:Chat(cmd)
 	local args = cmd.text:split(" ")
@@ -32,7 +32,6 @@ function Announce:Chat(cmd)
 		end
 
 		local time = tonumber(args[2])
-
 		local message = table.concat(args, " ", 3)
 
 		if #args < 2 then
